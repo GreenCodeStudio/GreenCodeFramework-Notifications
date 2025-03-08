73dcea6e39cf97c6638b1449d2b925ffeb5dbf90
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2025-03-08 17:49:05 +0100

    .

--- Notifications.php
+++ Notifications.php
@@ -68,6 +68,35 @@ class Notifications
         }
     }
 
+    function pushToFirebase($notification)
+    {
+        $url = 'https://fcm.googleapis.com/fcm/send';
+        $fields = array(
+            'to' => '/topic/main',
+            'notification' => array(
+                'title' => $title,
+                'body' => $body
+            )
+        );
+        $headers = array(
+            'Authorization: key=' . $_ENV['firebase_push_key'],
+            'Content-Type: application/json'
+        );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return $result;
+    }
+
+
     public function getForCurrentUser()
     {
         return $this->getForUser(\Authorization\Authorization::getUserData()->id);
